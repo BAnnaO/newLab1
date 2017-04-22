9d465d0eac2737c42738651ff6547d3f51253171
boguslavska <boguslavska_anna@mail.ru>
2017-04-22 23:03:24 +0300

    First iteration

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResearchTeam MyTeam = new ResearchTeam();
+            Console.WriteLine(string.Format("{0} {1} {2}",MyTeam[TimeFrame.Long].ToString(),MyTeam[TimeFrame.Year].ToString(),MyTeam[TimeFrame.TwoYears].ToString()));
+            MyTeam.Organisation = "SomeOrg";
+            MyTeam.RegistrNumber = 7;
+            MyTeam.Theme = "Stupid theme";
+            MyTeam.TimeDuration = TimeFrame.Year;
+            MyTeam.getPaper = new Paper[3] { new Paper("Cool paper", new Person(), new DateTime(2016, 11, 21)),new Paper(),new Paper()};
+            Paper[] Additive = new Paper[2] { new Paper("Cool paper 2", new Person(), new DateTime(2017, 05, 11)), new Paper() };
+            MyTeam.AddPapers(Additive);
+            Console.WriteLine(MyTeam.LastPaper);
+            Paper[] OneDim = new Paper[6] { new Paper(), new Paper(), new Paper(), new Paper(), new Paper(), new Paper() };
+            Paper[,] TwoDim = new Paper[2,3] { { new Paper(), new Paper(), new Paper() }, { new Paper(), new Paper(), new Paper() } };
+            Paper[][] Jagged = new Paper[2][];
+            Jagged[0] = new Paper[2] {new Paper(), new Paper() };
+            Jagged[1] = new Paper[3] { new Paper(), new Paper(), new Paper() };
+            int OneDimTime = Environment.TickCount;
+            foreach (Paper sp in OneDim) {
+                int k = 0;
+                while (k < 100000)
+                {
+                    sp._Author = new Person("Ann", "Boguslavska", new DateTime(1992, 01, 13));
+                    k++;
+                }
+            }
+            Console.WriteLine((Environment.TickCount-OneDimTime).ToString());
+
+            int TwoDimTime = Environment.TickCount;
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int k = 0;
+                    while (k < 100000)
+                    {
+                        TwoDim[i, j]._Author = new Person("Ann", "Boguslavska", new DateTime(1992, 01, 13));
+                        k++;
+                    }
+                }
+            }
+            Console.WriteLine((Environment.TickCount - OneDimTime).ToString());
+            int JaggedTime = Environment.TickCount;
+            for (int i = 0; i < Jagged.GetLength(0); i++)
+            {
+                for (int j = 0; j <Jagged[i].Length; j++)
+                {
+                    int k = 0;
+                    while ( k < 100000) {
+                        Jagged[i][j]._Author = new Person("Ann", "Boguslavska", new DateTime(1992, 01, 13));
+                        k++;
+                    }
+                    
+                }
+            }
+            Console.WriteLine((Environment.TickCount - JaggedTime).ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ResearchTeam.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newLab1
+{
+    class ResearchTeam
+    {
+        private string _InvestigationTheme;
+        public string Theme
+        { get { return _InvestigationTheme; }
+            set { _InvestigationTheme=value; }
+        }
+        private string _Organisation;
+        public string Organisation {
+            get { return _Organisation; }
+            set { _Organisation = value; }
+        }
+        private int _RegistrationNumber;
+        public int RegistrNumber {
+            get { return _RegistrationNumber; }
+            set { _RegistrationNumber = value; }
+        }  
+        private TimeFrame _InvestigationDuration;
+        public TimeFrame TimeDuration {
+            get { return _InvestigationDuration; }
+            set { _InvestigationDuration = value; }
+        }
+        private Paper[] _SetOfPublication;
+        public Paper[]  getPaper{
+            get { return _SetOfPublication; }
+            set { _SetOfPublication = value; }
+        }
+        public ResearchTeam(string InvestigationTheme, string Organisation, int RegistrationNumber, TimeFrame InvestigationDuration) {
+            _InvestigationDuration = InvestigationDuration;
+            _InvestigationTheme = InvestigationTheme;
+            _RegistrationNumber = RegistrationNumber;
+            _Organisation = Organisation;
+        }
+        public ResearchTeam():this("Chaos","IRE",13,TimeFrame.Year) {}
+        public Paper LastPaper {
+            get
+            {
+                if (_SetOfPublication != null)
+                {
+                    DateTime Maxdate = _SetOfPublication[0]._TimeOfPublication;
+                    int MaxdateIndex = 0;
+                    for (int i = 0; i < _SetOfPublication.Length; i++)
+                    {
+                        if (_SetOfPublication[i]._TimeOfPublication > Maxdate)
+                        {
+                            MaxdateIndex = i;
+                        }
+                    }
+                    return _SetOfPublication[MaxdateIndex];
+                }
+                else { return null; }
+            }
+        }
+        public bool this[TimeFrame SomeTimeFrame] {
+            get {
+                if (SomeTimeFrame == _InvestigationDuration)
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+        }
+        public void AddPapers(params Paper[] Somepaper) {
+            if (_SetOfPublication == null)
+            {
+                _SetOfPublication =  Somepaper ;
+            }
+            else {
+                Paper[] newArr = new Paper[_SetOfPublication.Length + Somepaper.Length];
+                Array.Copy(_SetOfPublication,newArr,_SetOfPublication.Length);
+                Array.Copy(Somepaper, 0, newArr, _SetOfPublication.Length, Somepaper.Length);
+                _SetOfPublication = newArr;
+
+            }
+            
+        }
+        public override string ToString()
+        {
+            string PublicationString = "";
+            foreach (Paper sp in _SetOfPublication) {
+                PublicationString += sp.ToString()+"\n";
+            }
+            return string.Format(_InvestigationTheme,"\n",_Organisation,
+                " ",_InvestigationDuration.ToString(),"\n",_RegistrationNumber.ToString(),"\n",
+                _InvestigationDuration.ToString()+"\n"+PublicationString);
+        }
+        public string ToShortString() {
+            return string.Format(_InvestigationTheme, "\n", _Organisation,
+                " ", _InvestigationDuration.ToString(), "\n", _RegistrationNumber.ToString(), "\n",
+                _InvestigationDuration.ToString() + "\n");
+        }
+    }
+}
